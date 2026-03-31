8c802a0adeeec19e8e509f083e6df0523150f09c
Prakash <prakash@factotumglobal.com>
2026-03-31 14:14:45 +0530

    fix: unoptimized images for external URLs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'fakestoreapi.com',
-        pathname: '/img/**',
       },
       {
         protocol: 'https',
