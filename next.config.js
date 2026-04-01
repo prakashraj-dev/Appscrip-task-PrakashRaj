a5d5485f74b0815522da381f80fe0db132886fbf
Prakash <prakash@factotumglobal.com>
2026-04-01 10:53:31 +0530

    fix: use reliable unsplash images

--- next.config.js
+++ next.config.js
@@ -3,11 +3,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'fakestoreapi.com' },
     ],
   },
 };
-
 
 module.exports = nextConfig;
